a6bfc85c18e7e8aa7200d6c2a1f57f3513425e80
qiq7qi <92316179+qiq7qi@users.noreply.github.com>
2025-07-04 12:04:16 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/score-checker/', // ← GitHub Pages 部署路径
-})
